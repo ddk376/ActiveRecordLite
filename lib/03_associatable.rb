cf583b387164e1169ad825d70f474d73cc558b86
Gina <ddk376@gmail.com>
2016-01-31 00:27:03 -0500

    class AssocOptions

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,32 +10,35 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    foreign_key =
-    class_name =
-    primary-key =
+    @foreign_key = options[:foreign_key] || (name.to_s + "_id").to_sym
+    @class_name = options[:class_name] || name.to_s.camelcase
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || (self_class_name.downcase.to_s + "_id").to_sym
+    @class_name = options[:class_name] || name.to_s.camelcase.singularize
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    
+    @options = BelongsToOptions.new
   end
 
   def has_many(name, options = {})
